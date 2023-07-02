a10b6468a753caca6b988937080967b882664d07
eduinfosite <frogjump721@gmail.com>
2023-07-02 20:46:26 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFeE8wU5GkDFTsS5VP8rKyahAND1_XlsI",
+  authDomain: "ema-john-with-firebase-77338.firebaseapp.com",
+  projectId: "ema-john-with-firebase-77338",
+  storageBucket: "ema-john-with-firebase-77338.appspot.com",
+  messagingSenderId: "820459628958",
+  appId: "1:820459628958:web:982e14eaabf31f1ed88e8f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
